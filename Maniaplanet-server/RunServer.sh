5d203960770cb5d9a595159cf5cd9b292f2eadd5
Louis Lacoste <louislacoste33@gmail.com>
2021-05-04 10:26:25 +0200

    rollback

--- Maniaplanet-server/RunServer.sh
+++ Maniaplanet-server/RunServer.sh
@@ -21,12 +21,14 @@ xmlstarlet ed --inplace -u "dedicated/server_options/max_players" -v "$SERVER_MA
 xmlstarlet ed --inplace -u "dedicated/server_options/password" -v "$SERVER_PASSWORD" UserData/Config/dedicated_conf.txt
 xmlstarlet ed --inplace -u "dedicated/server_options/max_spectators" -v "$SERVER_MAX_SPECTATORS" UserData/Config/dedicated_conf.txt
 xmlstarlet ed --inplace -u "dedicated/server_options/password_spectator" -v "$SERVER_SPECTATORS_PASSWORD" UserData/Config/dedicated_conf.txt
+xmlstarlet ed --inplace -u "dedicated/system_config/server_port" -v "$SERVER_PORT" UserData/Config/dedicated_conf.txt
+xmlstarlet ed --inplace -u "dedicated/system_config/server_p2p_port" -v "$SERVER_P2P_PORT" UserData/Config/dedicated_conf.txt
+xmlstarlet ed --inplace -u "dedicated/system_config/xmlrpc_port" -v "$SERVER_XMLRPC_PORT" UserData/Config/dedicated_conf.txt
 xmlstarlet ed --inplace -u "dedicated/system_config/title" -v "$SERVER_TITLE_PACK" UserData/Config/dedicated_conf.txt
 
 /opt/maniaplanet/ManiaPlanetServer /nodaemon \
   /dedicated_cfg=dedicated_conf.txt \
   /game_settings=$MatchSettings \
-  /forceip=$EXTERNAL_IP:$SERVER_PORT
 
 # xml ed --inplace -u "maniacontrol/server/port" -v "$SERVER_XMLRPC_PORT" ManiaControl/configs/server.xml
 # xml ed --inplace -u "maniacontrol/database/host" -v "$MANIACONTROL_DATABASE_HOST" ManiaControl/configs/server.xml
